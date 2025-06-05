c730433c85ba9a9f13cc262c8649f83fc913b56c
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-06-05 16:55:38 +0500

    "Add initial styles and scripts for input formatting and UI enhancements"

---
+++ js/register.js
@@ -0,0 +1,32 @@
+'use strict';
+
+(async _ => {
+  const statistics = new Statistics();
+  let user = JSON.parse(localStorage.getItem('user'));
+
+  document.querySelector('#joinToChannel')?.addEventListener('click', async () => {
+    await statistics.onClickTgBtn();
+    window.location.href = 'https://t.me/+2rQsuMRRGGZhMzAy';
+  })
+
+  if (user && user?.name && user?.phone && user?.time) {
+    const formData = new FormData();
+
+    formData.append('Ismi', user?.name);
+    formData.append('Telefon raqami', user?.phone);
+    formData.append(`Ro'yxatdan o'tgan vaqti`, user?.time);
+    formData.append(`Foydalanuvchi ID`, statistics.userId);
+    formData.append(`Timestamp`, statistics.time?.toString());
+
+    let response = await fetch('https://script.google.com/macros/s/AKfycbw76uh58b83Oa9Q6LTQsRZE8QvBwBk9WLxVSbTD2omnDYLYCn7UeVh7SLsDEs87jTnC/exec', {
+      method: 'POST',
+      body: formData
+    })
+    await response.json();
+    await statistics.onRegister(user);
+    localStorage.removeItem('user');
+  } else {
+    localStorage.removeItem('user');
+  }
+})()
+
